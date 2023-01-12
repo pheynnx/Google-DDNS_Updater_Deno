a6d511a2d11b1f6ca6d103a9fee69232f971f304
Eric Christensen <hebrew.8.12@gmail.com>
2023-01-11 20:06:15 -0800

    01/11/23 20:06

--- Google-DDNS.ts
+++ Google-DDNS.ts
@@ -2,15 +2,15 @@
 // Version 0.0.2
 // Last updated: 7/13/2020
 
-import * as base64 from "https://denopkg.com/chiefbiiko/base64/mod.ts";
-import * as Colors from "https://deno.land/std/fmt/colors.ts";
+import * as base64 from 'https://denopkg.com/chiefbiiko/base64/mod.ts';
+import * as Colors from 'https://deno.land/std/fmt/colors.ts';
 
 // Array of Google Dynamic DNS credentials
-import domains from "./domains.ts";
+import domains from './domains.ts';
 // Example domains.ts file
 
 // Sends GET request for public IP
-const response = await fetch("https://domains.google.com/checkip");
+const response = await fetch('https://domains.google.com/checkip');
 // Parses GET response as plain text; contains public IP
 const publicIp = await response.text();
 
@@ -19,20 +19,18 @@ for (let i = 0; i < domains.length; i++) {
   // Google requires base64 authentication; encodes username and password
   // Authentication format = 'Basic base64[username:password]'
   const encoded = base64.fromUint8Array(
-    new TextEncoder().encode(`${domains[i].username}:${domains[i].password}`),
+    new TextEncoder().encode(`${domains[i].username}:${domains[i].password}`)
   );
 
   // Sends POST request using authentication; public IP is set from URL query
   const setResponse = await fetch(
-    `https://domains.google.com/nic/update?hostname=${
-      domains[i].hostname
-    }&myip=${publicIp}`,
+    `https://domains.google.com/nic/update?hostname=${domains[i].hostname}&myip=${publicIp}`,
     {
-      method: "POST",
+      method: 'POST',
       headers: {
         Authorization: `Basic ${encoded}`,
       },
-    },
+    }
   );
 
   // Parses POST response as plain text
@@ -40,39 +38,37 @@ for (let i = 0; i < domains.length; i++) {
 
   // Switch statment for console feedback
   switch (updateResponse) {
-    case "nohost":
-    case "badauth":
-    case "notfqdn":
-    case "badagent":
-    case "abuse":
-    case "911":
-    case "conflict A":
-    case "conflict AAAA":
+    case 'nohost':
+    case 'badauth':
+    case 'notfqdn':
+    case 'badagent':
+    case 'abuse':
+    case '911':
+    case 'conflict A':
+    case 'conflict AAAA':
       console.log(
-        `${Colors.blue(domains[i].hostname)}: ${Colors.red(updateResponse)}`,
+        `${Colors.blue(domains[i].hostname)}: ${Colors.red(
+          updateResponse
+        )} ${new Date().toLocaleString()}`
       );
       break;
 
     case `nochg ${publicIp}`:
       console.log(
-        `${Colors.blue(domains[i].hostname)}: ${
-          Colors.yellow(
-            updateResponse,
-          )
-        }`,
+        `${Colors.blue(domains[i].hostname)}: ${Colors.yellow(
+          updateResponse
+        )} ${new Date().toLocaleString()}`
       );
       break;
     default:
       console.log(
-        `${Colors.blue(domains[i].hostname)}: ${
-          Colors.green(
-            updateResponse,
-          )
-        }`,
+        `${Colors.blue(domains[i].hostname)}: ${Colors.green(
+          updateResponse
+        )} ${new Date().toLocaleString()}`
       );
       break;
   }
 }
 
 // Clears console color without new line
-Deno.stdout.write(new TextEncoder().encode(Colors.reset("")));
+Deno.stdout.write(new TextEncoder().encode(Colors.reset('')));
